3eb085069ed0ea3d6105f6317e428489769430f3
BreakLunch <elias.ibanez@tecsup.edu.pe>
2023-06-26 17:39:21 -0500

    aluya

---
+++ lib/screens/card_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
+
+class CardScreen extends StatelessWidget {
+  const CardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Widget'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: ListView(
+          children: const [
+          CustomCardType1(),
+          SizedBox(height: 10),
+          CustomCardType1(),
+          SizedBox(height: 10),
+          CustomCardType1(),
+          SizedBox(height: 10),
+          CustomCardType1(),
+          SizedBox(height: 10),
+          CustomCardType1(),
+          SizedBox(height: 50),
+        ],
+        ), 
+      ),
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'package:fl_components_student/widgets/custom_card_type_1.dart';
+
