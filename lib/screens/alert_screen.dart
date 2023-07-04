3796e45c38c86312e800e6444ae89fe7054e82a6
BreakLunch <elias.ibanez@tecsup.edu.pe>
2023-07-03 20:26:18 -0500

    se

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
-  
+
+  void display(BuildContext context) {
+    showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('Mensaje de alerta'),
+              content: const Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Esto es el contenido interno de la alerta que hicimos'),
+                  SizedBox(height: 18),
+                  FlutterLogo(size: 200)
+                ],
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text(
+                      'Cancelar',
+                      style: TextStyle(fontSize: 20),
+                    ))
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+        child: ElevatedButton(
+            onPressed: () => display(context),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              child: Text(
+                'Procesar Alerta',
+                style: TextStyle(fontSize: 23),
+              ),
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.outgoing_mail),
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -14,18 +14,26 @@ class CardScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: ListView(
           children: const [
-          CustomCardType1(),
-          SizedBox(height: 10),
-          CustomCardType1(),
-          SizedBox(height: 10),
-          CustomCardType1(),
-          SizedBox(height: 10),
-          CustomCardType1(),
-          SizedBox(height: 10),
-          CustomCardType1(),
-          SizedBox(height: 50),
-        ],
-        ), 
+            CustomCardType3(),
+            SizedBox(height: 10),
+            CustomCardType4(),
+            SizedBox(height: 10),
+            CustomCardType5(
+                descripcion: 'Operacion Overlord',
+                imagenURL:
+                    'https://pics.filmaffinity.com/Overlord-562279452-large.jpg'),
+            SizedBox(height: 10),
+            CustomCardType5(
+                imagenURL:
+                    'https://www.eltiempo.com/uploads/2019/11/16/5dd08616d8370.jpeg'),
+            SizedBox(height: 10),
+            CustomCardType5(
+                descripcion: 'Jurassic Park',
+                imagenURL:
+                    'https://www.universalorlando.com/webdata/k2/es/us/files/Images/gds/jurassic-park-unipics-logo-b.jpg'),
+            SizedBox(height: 10)
+          ],
+        ),
       ),
     );
   }
